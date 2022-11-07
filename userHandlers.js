c89bbc6a70929adf60f31131d445a377edc46092
Milimaks <110846217+Milimaks@users.noreply.github.com>
2022-11-07 17:09:36 +0100

    fourth

--- userHandlers.js
+++ userHandlers.js
@@ -63,7 +63,7 @@ const updateUser = (req, res) => {
         })
         .catch((err) => {
             console.error(err);
-            res.status(500).send("Error editing the movie");
+            res.status(500).send("Error editing the user");
         });
 };
 

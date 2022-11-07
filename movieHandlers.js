5e5e31f1f125564281ad517797e6f5cbfd3fb7b2
Milimaks <110846217+Milimaks@users.noreply.github.com>
2022-11-07 17:23:02 +0100

    Cinquième

--- movieHandlers.js
+++ movieHandlers.js
@@ -96,13 +96,24 @@ const getMovieById = (req, res) => {
     });
 
 
-  const movie = movies.find((movie) => movie.id === id);
+};
 
-  if (movie != null) {
-    res.json(movie);
-  } else {
-    res.status(404).send("Not Found");
-  }
+const deleteMovie = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("delete from movies where id = ?", [id])
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error deleting the movie");
+    });
 };
 
 
@@ -114,4 +125,5 @@ module.exports = {
   getMovieById,
   postMovie,
   updateMovie,
+  deleteMovie,
 };
--- userHandlers.js
+++ userHandlers.js
@@ -112,12 +112,24 @@ const getUserById = (req, res) => {
             console.error(err);
             res.status(500).send("Error retrieving data from database");
         });
-
-
-
-
 };
+const deleteUser = (req, res) => {
+    const id = parseInt(req.params.id);
 
+    database
+        .query("delete from users where id = ?", [id])
+        .then(([result]) => {
+            if (result.affectedRows === 0) {
+                res.status(404).send("Not Found");
+            } else {
+                res.sendStatus(204);
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send("Error deleting the movie");
+        });
+};
 
 
 module.exports = {
@@ -125,4 +137,5 @@ module.exports = {
     getUsers,
     postUser,
     updateUser,
+    deleteUser,
 };
